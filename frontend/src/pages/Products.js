7d185b9ed2efd53351b21cc8d4f22fa0c48d32d9
kyaloeriq <kyaloeriq@gmail.com>
2024-10-08 11:02:18 -0700

    Connects Frontend to Backend

---
+++ frontend/src/pages/Products.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Products = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/products')
+            .then((res) => setProducts(res.data))
+            .catch((err) => console.error(err));
+    }, []);
+
+    return (
+        <div>
+            <h1>Products</h1>
+            <ul>
+                {products.map(product => (
+                    <li key={product._id}>{product.name} - ${product.price}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Products;
