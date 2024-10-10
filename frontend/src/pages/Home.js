6e294d0b17ec728195a418d4831c972a570f0969
kyaloeriq <kyaloeriq@gmail.com>
2024-10-10 00:28:45 -0700

    Home page

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+    <div>
+        <h1>Welcome to Our Shop</h1>
+        <p>Find the best products here!</p>
+    </div>
+);
+
+export default Home;
