69ce9b86aa88237e995a0393ba6af18e37ab3f53
kyaloeriq <kyaloeriq@gmail.com>
2024-10-10 07:29:38 -0700

    App.js update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Products from './pages/Products';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
@@ -7,11 +7,11 @@ import Cart from './pages/Cart';
 function App() {
     return (
         <Router>
-            <Switch>
+            <Routes>
                 <Route exact path="/" component={Home} />
                 <Route path="/products" component={Products} />
                 <Route path="/cart" component={Cart} />
-            </Switch>
+            </Routes>
         </Router>
     );
 }
