1a84d5aea30de254183f694fe8ef3b5d3d649b61
kyaloeriq <kyaloeriq@gmail.com>
2024-10-10 06:56:10 -0700

    renders the React app

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Optional, you can create an index.css file for styling
+import App from './App'; // Ensure there's an App component created
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
